831194d878252f1c9cbfb6ecf06fff4f0770abef
MichelaCap <capece.1702284@studenti.uniroma1.it>
2021-11-08 19:11:33 +0100

    chore: button for upload file

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-
+document.getElementById("formFile").onchange = function () {
+    var str = document.getElementById("formFile").value
+    var splitString = str.split('\\')
+    var name = splitString[2]
+    console.log(name)
+};
