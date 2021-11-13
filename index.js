fe07ce81ea6f977a3448a3b7f4779e9b7ff3b387
MichelaCap <capece.1702284@studenti.uniroma1.it>
2021-11-13 17:57:29 +0100

    del: delete file from repo

--- index.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("formFile").onchange = function () {
-    var str = document.getElementById("formFile").value
-    var splitString = str.split('\\')
-    var name = splitString[2]
-    console.log(name)
-};
