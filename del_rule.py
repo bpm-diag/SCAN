6b65f1d95136524f4dc5019fd21c039b55ccb77a
MichelaCap <capece.1702284@studenti.uniroma1.it>
2021-11-18 23:37:53 +0100

    chore: remove precedence rule

--- del_rule.py
+++ del_rule.py
@@ -193,3 +193,24 @@ def rule_del_chain_response():
     writeOnSegmentFile(segments)   
     writeOnRemoveSegmentFile(remove)
     return segments, remove
+
+def rule_del_precedence():
+    a = request.form["act1"]
+    b = request.form["act2"]
+    segments = takeSegmentFromFile()
+    removeSegment = takeRemoveSegmentFromFile() 
+    remove = []
+    for act in removeSegment:
+        if a in act and b in act:
+            position_a = act.index(a)
+            position_b = act.index(b)     
+            if position_b < position_a:
+                segments.append(act) 
+        elif b in act and a not in act:
+            segments.append(act)  
+        else : 
+            if act in segments:
+                remove.append(act)     
+    writeOnSegmentFile(segments)   
+    writeOnRemoveSegmentFile(remove)
+    return segments, remove
